433f3059a744a81da0e65f87a61e4c453b4ec921
vladislav <balaban-2000@list.ru>
2021-01-24 17:15:04 +0200

    Tests to learn git bash with code

---
+++ new_test_fle.cs
@@ -0,0 +1,15 @@
+using System;
+namespace ConsoleApplication3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            NumberManip num = new NumberManip();
+            Console.WriteLine(num.FindMax(10,30));
+            Console.WriteLine(num.FindMax(anime,not anime));
+
+        }
+
+    }
+}
